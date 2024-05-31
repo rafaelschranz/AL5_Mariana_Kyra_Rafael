1fedd6918095b305f445bbe780b51d873b5850e1
Rafael Schranz <106320220+rafaelschranz@users.noreply.github.com>
2024-05-31 10:52:41 +0200

    updates

--- js/common.js
+++ js/common.js
@@ -1,1 +1,10 @@
-/* JS Code, der nur auf mehreren Pages verwendet wird */
+document.addEventListener("DOMContentLoaded", () => {
+  const currentLocation = location.pathname;
+  const menuItem = document.querySelectorAll(".nav-link");
+  const menuLength = menuItem.length;
+  for (let i = 0; i < menuLength; i++) {
+    if (menuItem[i].getAttribute("href") === currentLocation) {
+      menuItem[i].classList.add("active");
+    }
+  }
+});
